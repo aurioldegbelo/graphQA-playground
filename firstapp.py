2572fc13cf68574cc9e31da0dbf79a483b24acec
Auriol Degbelo <auriol.degbelo@tu-dresden.de>
2024-03-30 15:48:54 +0000

    a commit

--- firstapp.py
+++ firstapp.py
@@ -1,5 +1,30 @@
 # pip install steamlit
 
 import streamlit as st
-x = st.slider("Select a value")
-st.write(x, "squared is", x * x)
+import numpy as np
+from timeit import default_timer as timer
+from langchain.graphs
+
+
+def query_graph (user_input):
+    pass
+
+
+
+
+st.set_page_config(layout="wide")
+title_col, empty_col, img_col = st.columns([2, 1, 2])    
+
+with title_col:
+    st.title("Question Answering Assistant")
+with img_col:
+    st.image("https://www.uni-muenster.de/imperia/md/images/geoinformatics/_v/2021-logo-ifgi-text-de.png", width=200)
+
+with st.chat_message("user"):
+     st.write("Hello 👋")
+     
+user_input = st.chat_input("Ask your question")
+if user_input:
+    st.write(f"User has sent the following question: {user_input}")
+    start = timer()
+    result = query_graph(user_input)
