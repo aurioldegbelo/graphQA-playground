cdb30f3e08f7c13fc85129783031e49da607b6ad
Auriol Degbelo <auriol.degbelo@tu-dresden.de>
2024-03-30 16:16:34 +0000

    basic structure of the app in place

--- firstapp.py
+++ firstapp.py
@@ -2,17 +2,29 @@
 # pip install --upgrade --quiet  langchain langchain-community langchain-openai neo4j
 
 import streamlit as st
-import numpy as np
+import os
 from timeit import default_timer as timer
-from langchain.graphs
+from langchain.chains import GraphCypherQAChain
+from langchain_openai import ChatOpenAI
+from langchain_community.graphs import Neo4jGraph
 
+# initialize environment variables
+openai_api_key = os.getenv("OPENAI_KEY")
+neo4j_connection_url = os.getenv("NEO4J_CONNECTION_URL")
+neo4j_username = os.getenv("NEO4J_USERNAME")
+neo4j_password = os.getenv("NEO4J_PASSWORD")
 
+# create access to the neo4j graph
+graph = Neo4jGraph(url=neo4j_connection_url, username=neo4j_username, password=neo4j_password)
+
+# function to query the neo4j knowledge graph
 def query_graph (user_input):
-    pass
+    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, openai_api_key=openai_api_key)
+    chain = GraphCypherQAChain.from_llm(graph=graph, llm=llm, verbose=True)
+    response = chain.invoke({"query": user_input})
+    return response
 
-
-
-
+# frontend application using streamlit
 st.set_page_config(layout="wide")
 title_col, empty_col, img_col = st.columns([2, 1, 2])    
 
